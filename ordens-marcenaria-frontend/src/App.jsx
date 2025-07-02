588f4b70686f48fca6757a0ce88aa3d134f95a17
MateusBrtA <143241589+MateusBrtA@users.noreply.github.com>
2025-07-02 15:05:10 -0300

    po

--- ordens-marcenaria-frontend/src/App.jsx
+++ ordens-marcenaria-frontend/src/App.jsx
@@ -14,8 +14,8 @@ import { ordersAPI, carpentersAPI } from './services/api.js';
 import './App.css';
 
 // Importar os modais
-import AddOrderModal from './components/AddOrderModal.jsx';
-import ManageCarpenterModal from './components/ManageCarpenterModal.jsx';
+import AddOrderModal from './components/ui/AddOrderModal.jsx';
+import ManageCarpenterModal from './components/ui/ManageCarpenterModal.jsx';
 import EditOrderModal from './components/EditOrderModal.jsx';
 
 function MainApp() {
